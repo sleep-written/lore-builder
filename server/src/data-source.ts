1410cd0dab10679c3704a99a8d5c5c42fc54308e
sleep-written <sleep.written0000@gmail.com>
2024-10-18 21:19:39 -0300

    Changes: - Added a tsconfig.build.json to transpile without sourceMaps. - Changed the build script in package.json to transpile using   "tsconfig.build.json".

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,14 +1,17 @@
-import { pathResolve } from '@bleed-believer/path-alias';
-import { join } from 'path';
+import { pathResolve } from '@bleed-believer/path-alias/utils';
 import { DataSource } from 'typeorm';
+import { join } from 'path';
 
+const url = import.meta.url;
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: join(process.cwd(), './app.db'),
-    entities: [
-        pathResolve('./entities/*.ts')
-    ],
-    migrations: [
-        pathResolve('./migrations/*.ts')
-    ]
+    entities: pathResolve('./entities/*.ts', {
+        url,
+        multi: true
+    }),
+    migrations: pathResolve('./migrations/*.ts', {
+        url,
+        multi: true
+    }),
 });
